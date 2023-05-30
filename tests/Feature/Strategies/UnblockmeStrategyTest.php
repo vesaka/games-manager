fb255adb6fd6684439124e1b16ddf67f41730b4f
Veselin <vesakabgr@gmail.com>
2023-05-30 07:03:53 +0300

    run working test with game_id

--- tests/Feature/Strategies/UnblockmeStrategyTest.php
+++ tests/Feature/Strategies/UnblockmeStrategyTest.php
@@ -3,16 +3,11 @@
 namespace Vesaka\Games\Tests\Feature\Strategies;
 
 use Vesaka\Games\Catalogue\Unblockme;
-use App\Models\User;
-use Laravel\Sanctum\Sanctum;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request as HttpRequest;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Auth;
-use Vesaka\Games\Models\GameSession;
-use Vesaka\Games\DB\Seeders\PlayersSeeder;
+use Vesaka\Games\DB\Seeders\UnblockmeGameSessionsSeeder;
+use Vesaka\Games\Tests\Traits\BindsGameSessionRepository;
 
 /**
  * Description of UnblockmeStrategyTest
@@ -21,95 +16,34 @@ use Vesaka\Games\DB\Seeders\PlayersSeeder;
  */
 class UnblockmeStrategyTest extends TestCase {
 
-    use WithFaker,
-        RefreshDatabase;
-    
+    use BindsGameSessionRepository;
+
     protected function setUp(): void {
         parent::setUp();
-        // Mock the authenticated user
-        $user = User::factory()->create();
-        $this->actingAs($user);
-        
-        // Set the session key configuration
-        Config::set('games.session_key', 'session_id');
+        $this->bindGameSessionAlias();
     }
 
-    public function test_unblockme_starts_game_session() {
-        $request = $this->mockRequest();
-        
-       
-        $strategy = new Unblockme;
-        $gameSession = $strategy->begin($request);
-        $this->assertInstanceOf(GameSession::class, $gameSession);
-        $this->assertNotNull($gameSession->user_id);
-        $this->assertNotNull($gameSession->started_at);
-        $this->assertNotEmpty($gameSession->info);      
-        
-    }
-    
-    
-    public function test_unblockme_saves_game_session() {
-        $score = 250;
-        $strategy = new Unblockme();
-        $mockedGameSession = $strategy->begin($this->mockRequest());
-        $gameKey = config('games.session_key');
-        
-        $request = $this->mockRequest([
-            'score' => $score,
-            $gameKey => $mockedGameSession->id
-        ]);
-        
-        $gameSession = $strategy->save($request);
-        $this->assertInstanceOf(GameSession::class, $gameSession);
-        $this->assertEquals($request->score, $gameSession->score);
-        $this->assertNotNull($gameSession->ended_at);
-        
-        
-    }
-    
     public function test_unblockme_gets_top_scores() {
         $this->artisan('db:seed', [
-            '--class' => PlayersSeeder::class
+            '--class' => UnblockmeGameSessionsSeeder::class
         ]);
-        
+
+        $mockRequest = new HttpRequest();
+        $mockRequest->headers->set(HEADER_GAME_NAME, 'unblockme');
+        Request::swap($mockRequest);
+
         $strategy = new Unblockme();
-        
+
         $limit = 10;
-        
-        $topScores = $strategy->getRanking(10);
+        $topScores = $strategy->getRanking($limit);
         $currentScore = $topScores->get(0)->score;
-        
+
         $this->assertLessThanOrEqual($limit, $topScores->count());
-        
-        $topScores->each(function($game) use ($currentScore) {
+
+        $topScores->each(function ($game) use ($currentScore) {
             $this->assertIsInt($game->score);
-            $this->assertIsString($game->top_scores);
             $this->assertLessThanOrEqual($currentScore, $game->score);
-            
-            $bestScores = collect(explode(',', $game->top_scores));
-            $this->assertGreaterThanOrEqual(1, $bestScores->count());
-            
-            $currentScore = $game->score;
-            
-            $currentBestScore = $game->score;
-            $bestScores->each(function($score) use ($currentBestScore) {
-                $this->assertLessThanOrEqual($currentBestScore, (int) $score);
-                $currentBestScore = (int) $score;
-            });
-            
-            
         });
-        
-    }
-    
-    protected function mockRequest(array $data = []): Request
-    {
-        $request = Request::create('/', 'POST');
-        $request->merge($data);
-        $request->setUserResolver(function () {
-            return Auth::user();
-        });
-        return $request;
     }
 
 }
