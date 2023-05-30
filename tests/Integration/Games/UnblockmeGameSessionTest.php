2a6d2d56a36dd246316a3dfb955a024bfabfbb38
Veselin <vesakabgr@gmail.com>
2023-05-30 08:30:06 +0300

    added unblockme game session test class

---
+++ tests/Integration/Games/UnblockmeGameSessionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vesaka\Games\Tests\Integration\Games;
+
+
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+use Vesaka\Games\Tests\Traits\BindsGameSessionRepository;
+
+/**
+ * Description of UnblockmeGameSessionTest
+ *
+ * @author vesak
+ */
+class UnblockmeGameSessionTest extends TestCase {
+    
+    use BindsGameSessionRepository;
+    
+    protected function setUp(): void {
+        parent::setUp();
+        $this->bindGameSessionAlias();
+        Sanctum::actingAs(auth()->user());
+        
+    }
+    
+    public function test_start_unblockme_request_returns_200() {
+        $response = $this->withHeaders([
+            HEADER_GAME_NAME => 'unblockme'
+        ])->json('post', '/api/play/start');
+
+        $response->assertStatus(200);
+    }
+    
+//    public function test_end_unblockme_request_returns_200() {
+//        //$mockGameSession = 
+//    }
+}
