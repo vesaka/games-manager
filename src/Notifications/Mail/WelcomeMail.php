ac88ed82eed373fdd19075ec7fffab6cd875ba1d
Veselin <vesakabgr@gmail.com>
2023-06-30 10:00:47 +0300

    wlecome mail to spa

--- src/Notifications/Mail/WelcomeMail.php
+++ src/Notifications/Mail/WelcomeMail.php
@@ -4,7 +4,9 @@ namespace Vesaka\Games\Notifications\Mail;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
-
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Carbon;
 /**
  * Description of WelcomeMail
  *
@@ -23,5 +25,19 @@ class WelcomeMail extends VerifyEmail {
             ->subject(__('Verify Email Address'))
             ->line(__('Please click the button below to verify your email address.'))
             ->action(__('Verify Email Address'), $url);
+    }
+
+    protected function verificationUrl($notifiable)
+    {
+        $gameKey = request()->header(HEADER_GAME_NAME) ?? (defined('CLI_GAME_HEADER_NAME') ? CLI_GAME_HEADER_NAME : null);
+        return URL::temporarySignedRoute(
+            'game::player.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+                'game' => $gameKey
+            ]
+        );
     }
 }
