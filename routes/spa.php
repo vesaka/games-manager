7332981bd69f4d411a19e1d094dbfd6e4817447c
Veselin <vesakabgr@gmail.com>
2023-06-30 09:56:39 +0300

    aded reset passord route

--- routes/spa.php
+++ routes/spa.php
@@ -1,5 +1,11 @@
 <?php
 use Vesaka\Games\Http\Controllers\GameController;
+use Vesaka\Games\Http\Controllers\AuthController;
 
-Route::get('unblockme/{path?}/{any?}', [GameController::class, 'spa']);
+Route::get('unblockme/{path?}/{any?}', [GameController::class, 'spa'])->name('spa');
 
+Route::get('verify-email/{id}/{hash}/{game}', [AuthController::class, 'verifyEmail'])->name('player.verify');
+Route::get('{game}/reset-password', [GameController::class, 'spa'])->name('player.reset-password');
+
+
+Route::get('{game}/{path?}/{any?}', [GameController::class, 'spa'])->name('spa');
