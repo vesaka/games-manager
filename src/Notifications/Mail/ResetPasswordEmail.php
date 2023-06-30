20ee96f72f63608b5dabdaf46c126a09499d29d1
Veselin <vesakabgr@gmail.com>
2023-06-30 10:00:35 +0300

    resetUrl to spa

--- src/Notifications/Mail/ResetPasswordEmail.php
+++ src/Notifications/Mail/ResetPasswordEmail.php
@@ -3,6 +3,8 @@
 namespace Vesaka\Games\Notifications\Mail;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+
 /**
  * Description of ResetPasswordEmail
  *
@@ -18,10 +20,19 @@ class ResetPasswordEmail extends ResetPassword {
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->subject(__('Reset Password Notification'))
+            ->line(__('You are receiving this email because we received a password reset request for your account.'))
+            ->action(__('Reset Password'), $url)
+            ->line(__('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(__('If you did not request a password reset, no further action is required.'));
+    }
+
+    protected function resetUrl($notifiable)
+    {
+        return url(route('game::player.reset-password', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+            'game' => request()->header(HEADER_GAME_NAME) ?? (defined('CLI_GAME_HEADER_NAME') ? CLI_GAME_HEADER_NAME : null)
+        ], false));
     }
 }
