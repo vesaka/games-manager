7225f282e36b26bc86a1206d039db1a03b45eee1
Veselin <vesakabgr@gmail.com>
2023-09-08 04:24:08 +0300

    cleanup unused classes

--- src/Database/Interfaces/GameSessionInterface.php
+++ src/Database/Interfaces/GameSessionInterface.php
@@ -2,18 +2,22 @@
 
 namespace Vesaka\Games\Database\Interfaces;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use Vesaka\Core\Abstracts\BaseInterface;
 use Vesaka\Games\Models\GameSession;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 /**
+ *
  * @author Vesaka
  */
 interface GameSessionInterface extends BaseInterface {
+    
     public function start(Request $request): GameSession;
-
+    
     public function end(Request $request): GameSession;
-
+    
     public function getRankings(): Collection;
+    
 }
+
