b83e5e24a693c87162e8c83cb88edbb3ddc4d8a5
Veselin <vesakabgr@gmail.com>
2023-09-08 06:22:41 +0300

    use default settings

--- config/games.php
+++ config/games.php
@@ -5,8 +5,14 @@ use Vesaka\Games\Catalogue;
 return [
     'identifier' => '_gk',
     'session_key' => 'sid',
-    'allow_guest_sessions' => false,
+    'allow_guest_sessions' => true,
+    'guest_email_domain' => '@guest.com',
     'catalogue' => [
+        'default' => [
+            'strategy' => Catalogue\BaseGame::class,
+            'allow_guest_sessions' => true,
+            'guest_email_domain' => '@guest.com',
+        ],
         'unblockme' => [
             'strategy' => Catalogue\Unblockme::class,
         ],
