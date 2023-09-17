ecfd4178ea23dc436015759a78a6d26e44602389
Veselin <vesakabgr@gmail.com>
2023-09-17 20:37:36 +0300

    added hash and decryption

--- src/Models/GameSession.php
+++ src/Models/GameSession.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Model as AppModel;
 use Vesaka\Games\DB\Factories\GameSessionFactory;
-
+use Illuminate\Encryption\Encrypter;
 /**
  * Description of GameSession
  *
  * @author vesak
  */
 class GameSession extends AppModel {
+
     public const COMPLETED = 'completed';
 
     public const ACTIVE = 'active';
@@ -21,7 +22,10 @@ class GameSession extends AppModel {
 
     protected $table = 'game_sessions';
 
-    protected $fillable = ['game_id', 'user_id', 'started_at', 'ended_at', 'score', 'payload', 'info'];
+    protected $fillable = [
+        'game_id', 'user_id', 'started_at', 'ended_at',
+        'score', 'payload', 'info', 'hash', 'status'
+    ];
 
     protected $attributes = [
         'game_id' => 1,
@@ -36,6 +40,8 @@ class GameSession extends AppModel {
         'payload' => 'array',
         'info' => 'array',
     ];
+
+    protected array|null $data = null;
 
     protected static function newFactory(): Factory {
         return GameSessionFactory::new();
@@ -59,5 +65,29 @@ class GameSession extends AppModel {
 
     public function scopeFailed(Builder $query): void {
         $query->where('status', self::FAILED);
+    }
+
+    public function getData($encrypted = false): array|null {
+        if (!$this->data) {
+            $json = $this->payload;
+
+            if ($encrypted) {
+                $this->data = json_decode((new Encrypter($this->hash, 'aes-256-cbc'))->decrypt($this->payload, false), true);
+            } else {
+                $this->data = $json;
+            }
+        }
+        
+        return $this->data;
+    }
+
+    public function getPayload(string $attribute, $encrypted = false): null | int | string |array {
+        $data = $this->getData($encrypted);
+        if (isset($data[$attribute])) {
+            return $data[$attribute];
+        }
+
+        return null;
+
     }
 }
