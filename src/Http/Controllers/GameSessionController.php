361f8554212e6bd2776e8042dbe02c10e834044d
Veselin <vesakabgr@gmail.com>
2023-09-17 20:38:20 +0300

    added test encryption action

--- src/Http/Controllers/GameSessionController.php
+++ src/Http/Controllers/GameSessionController.php
@@ -4,17 +4,18 @@ namespace Vesaka\Games\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Encryption\Encrypter;
 /**
  * Description of GameSessionController
  *
  * @author vesak
  */
 class GameSessionController extends Controller {
+
     public function start(Request $request) {
         return app('game.session')
             ->start($request)
-            ->only('id', 'game_id', 'status');
+            ->only('id', 'game_id', 'hash', 'status');
     }
 
     public function end(Request $request) {
@@ -25,5 +26,9 @@ class GameSessionController extends Controller {
 
     public function leaderboard(Request $request) {
         return app('game.session')->getRankings($request);
+    }
+
+    public function testEncryption(Request $request) {
+        return (new Encrypter($request->secret, 'aes-256-cbc'))->decrypt($request->payload, false);
     }
 }
