2bbc1e430645f69d0ccb5229c042374022e4eb2c
Veselin <vesakabgr@gmail.com>
2023-09-18 04:19:21 +0300

    fix command typeings

--- src/Console/Commands/Player/PurgeGuestUsers.php
+++ src/Console/Commands/Player/PurgeGuestUsers.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Vesaka\Games\Console\Commands;
+namespace Vesaka\Games\Console\Commands\Player;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 
 class PurgeGuestUsers extends Command {
 
-    protected string $signature = 'purge:guests';
+    protected $signature = 'purge:guests';
 
-    protected string $description = 'Purge guest users';
+    protected $description = 'Purge guest users';
 
     public function handle() {
         $this->info('Purging guest users...');
