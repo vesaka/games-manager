fa6b473ee772a10a25418d197131881619497fbd
Veselin <vesakabgr@gmail.com>
2023-09-18 03:31:50 +0300

    fix missing verify routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Vesaka\Games\Http\Controllers\{GameController, AuthController};
+Route::get('verify-email/{id}/{hash}/{game}', [AuthController::class, 'verifyEmail'])->name('player.verify');
+Route::get('{game}/reset-password', [GameController::class, 'spa'])->name('player.reset-password');
 Route::name('game-app')->get('{name}/{path?}', 'GameController@app');
