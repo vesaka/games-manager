9d01558d1ada0014bf9d14401b79143954d9ca05
Veselin <vesakabgr@gmail.com>
2023-09-18 04:16:43 +0300

    checnge forgot password route path

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Vesaka\Games\Http\Controllers\GameSessionController;
 
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('register', [AuthController::class, 'register'])->name('register');
-Route::post('send-password-reset-link', [AuthController::class, 'sendPasswordResetLink'])->name('forgot-password');
+Route::post('forgot-password', [AuthController::class, 'sendPasswordResetLink'])->name('forgot-password');
 Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
 Route::get('leaderboard', [GameSessionController::class, 'leaderboard'])->name('leaderboard');
 
