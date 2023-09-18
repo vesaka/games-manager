4e802459ad3944d9d1a43d541a7a976bf144d22b
Veselin <vesakabgr@gmail.com>
2023-09-18 03:02:36 +0300

    transfer geust to player listener

---
+++ src/Listeners/TransferGuestToPlayer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Vesaka\Games\Listeners;
+
+use Vesaka\Games\Models\{Player, GameSession};
+use Illuminate\Auth\Events\Authenticated;
+
+class TransferGuestToPlayer
+{
+
+    public function handle(Authenticated $event): void
+    {
+
+        if (auth()->user()->isGuest) {
+            $guest = Player::find(auth()->id());
+            $player = Player::find($event->user->id);
+            app('game.session')->transfer($guest, $player);
+
+            auth()->logout();
+            $guest->delete();
+        }
+
+    }
+}
